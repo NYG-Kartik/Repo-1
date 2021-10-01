898ae76ded08daf8833dc4da840a15ef54712345
Kartik <kvanjani20@stuy.edu>
2021-09-30 20:16:41 -0400

    initsetup

--- Greet.java
+++ Greet.java
@@ -1,4 +1,10 @@
-public class Greet{
-public static void main(String[] args){
-system.out.println("No hablo queso");}}
-
+/*
+Kartik Vanjani
+APCS
+HW 8 Refractor Freshie Zero
+2021-09-30
+*/
+public class Greet {
+ public static void 
+  main(String[] args){
+System.out.println("No hablo queso");}}

84fb6170cca9929eb856bb86fc3a50b18f8931b2
Kartik <kvanjani20@stuy.edu>
2021-10-04 18:47:08 -0400

    initsetup

--- Greet.java
+++ Greet.java
@@ -1,10 +1,20 @@
 /*
-Kartik Vanjani
-APCS
-HW 8 Refractor Freshie Zero
-2021-09-30
+Kartik Vanjani, Ethan Lam, Krrish, Team Large Men
+APCS PD8
+HW 10 Refeactor Big Sib One
+2021-10-04
+Discoveries: You always need the systems.out.println command to have it
+print what you want it to print.
+Questions: I cannot figure out how to fix a coding error that keeps popping
+up and neither can my duo partner it is a confusing error message. What 
+does the carrot between void and main mean? Why won't it compile?
 */
 public class Greet {
- public static void 
-  main(String[] args){
-System.out.println("No hablo queso");}}
+    public static class void main(String[] args ){
+        System.out.println( BigSib.greet("Ethan") );
+        System.out.println( BigSib.greet("Kartik") );
+        System.out.println( BigSib.greet("Johnh") );
+    }
+}
+     public static void greet(String name) {
+           System.out.println("Hey, " + name + " , thanks for your help!");}}

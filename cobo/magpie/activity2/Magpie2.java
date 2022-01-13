0297ce0d169d5491ce41a9d3c5f7621c0bd781a0
Kartik Vanjani <90806469+NYG-Kartik@users.noreply.github.com>
2022-01-12 21:07:28 -0500

    Update Magpie2.java

--- cobo/magpie/activity2/Magpie2.java
+++ cobo/magpie/activity2/Magpie2.java
@@ -1,10 +1,10 @@
 /**
  * A program to carry on conversations with a human user.
- * This is the initial version that:  
+ * This is the initial version that:
  * <ul><li>
  *       Uses indexOf to find strings
  * </li><li>
- * 		    Handles responding to simple words and phrases 
+ * 		    Handles responding to simple words and phrases
  * </li></ul>
  * This version uses a nested if to handle default responses.
  * @author Laurie White
@@ -13,20 +13,25 @@
 public class Magpie2
 {
 	/**
-	 * Get a default greeting 	
+	 * Get a default greeting
 	 * @return a greeting
 	 */
 	public String getGreeting()
 	{
 		return "Hello, let's talk.";
 	}
-	
+
 	/**
 	 * Gives a response to a user statement
-	 * 
+	 *
 	 * @param statement
 	 *            the user statement
 	 * @return a response based on the rules given
+	 * The higher up a key word conditional is in the getResponse method, the
+	 * higher priority it has.
+	 * QUESTION 1: These statements have key words as part of other words, and so
+	 * return a response based on the key words even though they don't necessarily
+	 * make sense (eg. no in know & mother in smothered ).
 	 */
 	public String getResponse(String statement)
 	{
@@ -42,6 +47,27 @@ public class Magpie2
 		{
 			response = "Tell me more about your family.";
 		}
+		else if (statement.indexOf("cat") >= 0
+				|| statement.indexOf("dog") >= 0)
+		{
+			response = "Tell me more about your pets.";
+		}
+		else if (statement.indexOf("Mr. Mykolyk") >= 0)
+		{
+			response = "He sounds terrific.";
+		}
+		else if (statement.indexOf("Emily") >= 0)
+		{
+			response = "OMG!! I know her, too!";
+		}
+		else if (statement.indexOf("Kartik") >= 0)
+		{
+			response = "Hey, I've heard of him from somwhere!";
+		}
+		else if (statement.indexOf("Tasnim") >= 0)
+		{
+			response = "I think she is in my APCS class.";
+		}
 		else
 		{
 			response = getRandomResponse();
@@ -55,11 +81,11 @@ public class Magpie2
 	 */
 	private String getRandomResponse()
 	{
-		final int NUMBER_OF_RESPONSES = 4;
+		final int NUMBER_OF_RESPONSES = 6;
 		double r = Math.random();
 		int whichResponse = (int)(r * NUMBER_OF_RESPONSES);
 		String response = "";
-		
+
 		if (whichResponse == 0)
 		{
 			response = "Interesting, tell me more.";
@@ -75,6 +101,14 @@ public class Magpie2
 		else if (whichResponse == 3)
 		{
 			response = "You don't say.";
+		}
+		else if (whichResponse == 4)
+		{
+			response = "I really don't care, but you seem to. Continue.";
+		}
+		else if (whichResponse == 5)
+		{
+			response = "For real? That's so awesome.";
 		}
 
 		return response;

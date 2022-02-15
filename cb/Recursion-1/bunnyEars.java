39d94f305ea717a93dd727244949e2c8f2d77120
Kartik Vanjani <90806469+NYG-Kartik@users.noreply.github.com>
2022-02-14 20:29:43 -0500

    Update bunnyEars.java

--- cb/Recursion-1/bunnyEars.java
+++ cb/Recursion-1/bunnyEars.java
@@ -1,6 +1,13 @@
+public class bunnyEars {
 public int bunnyEars(int bunnies) {
   if (bunnies == 0) 
   return 0;
   return bunnies * 2;
 
+}
+public void main (String [] args) {
+	System.out.println(bunnyEars(1));
+	System.out.println(bunnyEars(2));
+	System.out.println(bunnyEars(3));
+}
 }

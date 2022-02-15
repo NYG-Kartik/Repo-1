3c2ce155c64ee02fa0153d792bd55020f9c2b986
Kartik Vanjani <90806469+NYG-Kartik@users.noreply.github.com>
2022-02-14 20:31:52 -0500

    Update sumDigits.java

--- cb/Recursion-1/sumDigits.java
+++ cb/Recursion-1/sumDigits.java
@@ -1,7 +1,14 @@
+public class sumDigits {
 public int sumDigits(int n) {
   if (n < 10)
   return 0;
   if (n == 10)
   return 10;
   return (n % 10) + sumDigits(n/10);
+}
+public void main (String [] args) {
+	System.out.println(sumDigits(1));
+	System.out.println(sumDigits(2));
+	System.out.println(sumDigits(3));
+}
 }

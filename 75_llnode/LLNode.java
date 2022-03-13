7c8de0f1c05390444aaf7d74a7d5e1bb28e10aba
Kartik Vanjani <90806469+NYG-Kartik@users.noreply.github.com>
2022-03-13 19:19:41 -0400

    Create LLNode.java

---
+++ 75_llnode/LLNode.java
@@ -0,0 +1,63 @@
+//Team Fred - Andrey Sokolov + Geese, Rin Fukuoka + Tape, Kartik Vanjani + Krish
+//APCS pd8
+//HW74 -- LLNode
+//2022-03-11w
+//time spent: 1h
+
+// DISCO
+// - This wasn't as hard as we though it was going to be!
+// - Despite feeling like it wouldn't work, using LLNode for cdr works like a charm.
+
+// QCC
+// - How exactly should the toString() return the nodes?
+// - Will we be able to rename this class at a later date?
+
+//Node - Implemented as foretold in the UML Design Doc
+public class LLNode{
+    private String cargo;
+    private LLNode cdr;
+    //----------------Constructors-----------------//
+    public LLNode(String cargo, LLNode cdr){
+        this.cargo = cargo;
+        this.cdr = cdr;
+    }
+
+    public LLNode(String cargo){
+        this.cargo = cargo;
+        this.cdr = null;
+    }
+    //--------------End Constructors---------------//
+
+    //------------------Mutators-------------------//
+    public void setCargo(String newCargo){
+        this.cargo = newCargo;
+    }
+
+    public void setNode(LLNode cdr){
+        this.cdr = cdr;
+    } 
+    //----------------End Mutators-----------------//
+
+    //------------------Accessors------------------//
+    public String getCargo(){
+        return this.cargo;
+    }
+    public LLNode getCdr(){
+        return this.cdr;
+    }
+    public String get(){
+        return "{ " + getCargo() + ", " + cdr.toString() + " }";
+    }
+
+    public String toString(){
+        return "{ " + getCargo() + ", " + getCdr() + " }";
+    }
+    //----------------End Accessors----------------//
+public class Driver{
+    public static void main(String[] args){
+        LLNode cool = new LLNode("haha");
+        LLNode okay = new LLNode("hoohoo", cool);
+        System.out.println(okay);
+    }
+}
+}
